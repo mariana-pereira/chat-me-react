06c77c8996c41ee37a2be00070fd874057ebbd09
Mariana Pereira <marianna.22sp@gmail.com>
2020-06-13 13:30:42 -0300

    Create routes for application

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App" />
-  );
-}
+import Routes from './routes';
+import history from './services/history';
+
+const App: React.FC = () => (
+  <Router history={history}>
+    <Routes />
+  </Router>
+);
 
 export default App;
