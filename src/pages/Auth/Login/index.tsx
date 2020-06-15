ce3216bceed1f93a410133f5ee188eca4934d384
Mariana Pereira <marianna.22sp@gmail.com>
2020-06-15 14:18:31 -0300

    Add unform

--- src/pages/Auth/Login/index.tsx
+++ src/pages/Auth/Login/index.tsx
@@ -1,28 +1,43 @@
-import React from 'react';
+import React, { InputHTMLAttributes } from 'react';
+import { SubmitHandler } from '@unform/core';
+import { Form } from '@unform/web';
+
+import Input from '../../../components/Input';
 
 import logo from '../../../assets/logo-light.svg';
 import {
   Container, Image, Auth, Logo,
 } from '../styles';
 
-const Login: React.FC = () => (
-  <Container>
-    <Image>
-      <Logo src={logo} />
-    </Image>
-    <Auth>
-      <form>
-        <input name="email" type="email" placeholder="Digite seu e-mail" />
-        <input
-          name="password"
-          type="password"
-          placeholder="Digite sua senha"
-        />
-        <button type="submit">Entrar</button>
+interface FormData extends InputHTMLAttributes<HTMLInputElement> {
+  email: string;
+  password: string;
+};
 
-      </form>
-    </Auth>
-  </Container>
-);
+const Login: React.FC = () => {
+  const handleSubmit: SubmitHandler<FormData> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <Container>
+      <Image>
+        <Logo src={logo} />
+      </Image>
+      <Auth>
+        <Form onSubmit={handleSubmit}>
+          <Input name="email" type="email" placeholder="Digite seu e-mail" />
+          <Input
+            name="password"
+            type="password"
+            placeholder="Digite sua senha"
+          />
+          <button type="submit">Entrar</button>
+
+        </Form>
+      </Auth>
+    </Container>
+  );
+};
 
 export default Login;
--- src/pages/Auth/Register/index.tsx
+++ src/pages/Auth/Register/index.tsx
@@ -1,29 +1,44 @@
-import React from 'react';
+import React, { InputHTMLAttributes } from 'react';
+import { SubmitHandler } from '@unform/core';
+import { Form } from '@unform/web';
+
+import Input from '../../../components/Input';
 
 import logo from '../../../assets/logo-light.svg';
 import {
   Container, Image, Auth, Logo,
 } from '../styles';
 
-const Register: React.FC = () => (
-  <Container>
-    <Image>
-      <Logo src={logo} />
-    </Image>
-    <Auth>
-      <form>
-        <input name="name" placeholder="Digite seu nome" />
-        <input name="email" type="email" placeholder="Digite seu e-mail" />
-        <input
-          name="password"
-          type="password"
-          placeholder="Digite sua senha"
-        />
-        <button type="submit">Cadastrar</button>
+interface FormData extends InputHTMLAttributes<HTMLInputElement> {
+  email: string;
+  password: string;
+};
 
-      </form>
-    </Auth>
-  </Container>
-);
+const Register: React.FC = () => {
+  const handleSubmit: SubmitHandler<FormData> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <Container>
+      <Image>
+        <Logo src={logo} />
+      </Image>
+      <Auth>
+        <Form onSubmit={handleSubmit}>
+          <Input name="name" placeholder="Digite seu nome" />
+          <Input name="email" type="email" placeholder="Digite seu e-mail" />
+          <Input
+            name="password"
+            type="password"
+            placeholder="Digite sua senha"
+          />
+          <button type="submit">Cadastrar</button>
+
+        </Form>
+      </Auth>
+    </Container>
+  );
+};
 
 export default Register;
