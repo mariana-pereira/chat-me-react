5131ace60f2898d0255fc9a38688e2fb5253e68b
Mariana Pereira <marianna.22sp@gmail.com>
2020-06-20 17:26:07 -0300

    Persist auth state

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Router } from 'react-router-dom';
 
 import Routes from './routes';
 
-import store from './store';
+import { store, persistor } from './store';
 import history from './services/history';
 
 import GlobalStyles from './styles/global';
 
 const App: React.FC = () => (
   <Provider store={store}>
-    <Router history={history}>
-      <Routes />
-      <GlobalStyles />
-    </Router>
+    <PersistGate persistor={persistor}>
+      <Router history={history}>
+        <Routes />
+        <GlobalStyles />
+      </Router>
+    </PersistGate>
   </Provider>
 );
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,8 @@
 import { createStore, applyMiddleware, Store } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import { AuthState } from './modules/auth/types';
 
 import rootReducer from './modules/rootReducer';
@@ -9,10 +12,20 @@ export interface ApplicationState {
   auth: AuthState;
 }
 
+const persistConfig = {
+  key: 'chat-app',
+  storage,
+  whitelist: ['auth', 'user'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const sagaMiddleware = createSagaMiddleware();
 
-const store: Store<ApplicationState> = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+const store: Store<ApplicationState> = createStore(persistedReducer, applyMiddleware(sagaMiddleware));
 
 sagaMiddleware.run(rootSaga);
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };
