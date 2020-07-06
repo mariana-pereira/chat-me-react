b845a44750180c8f604476e380e25958861fbbdc
Mariana Pereira <marianna.22sp@gmail.com>
2020-07-06 19:43:15 -0300

    Add field validation to login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -3,14 +3,15 @@ import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { SubmitHandler } from '@unform/core';
 import { Form } from '@unform/web';
+import { toast } from 'react-toastify';
 
 import { ApplicationState } from '../../store';
-
 import { signInRequest } from '../../store/modules/auth/actions';
 
 import Input from '../../components/Input';
-
 import Layout from '../../Layouts/Auth';
+
+import schema from './schema';
 
 interface FormData extends InputHTMLAttributes<HTMLInputElement> {
   email: string;
@@ -21,8 +22,14 @@ const Login: React.FC = () => {
   const dispatch = useDispatch();
   const loading = useSelector((state: ApplicationState) => state.auth.loading);
 
-  const handleSubmit: SubmitHandler<FormData> = (data) => {
-    dispatch(signInRequest(data));
+  const handleSubmit: SubmitHandler<FormData> = async (data) => {
+    try {
+      await schema.validate(data);
+
+      dispatch(signInRequest(data));
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
 
   return (
