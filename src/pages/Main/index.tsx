24eff0a584b067aca4e429ba2208484e94acbf43
Mariana Pereira <marianna.22sp@gmail.com>
2020-10-19 13:52:53 -0300

    Connect to server through socket

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import io from 'socket.io-client';
 
 import ContactList from '../../components/ContactList';
 import Conversation from '../../components/Conversation';
 
 import { Container } from './styles';
 
-const Main: React.FC = () => (
-  <Container>
-    <div className="left">
-      <ContactList />
-    </div>
-    <div className="right">
-      <Conversation />
-    </div>
-  </Container>
-);
+let socket;
+
+const Main: React.FC = () => {
+  const ENDPOINT = 'http://localhost:3333';
+
+  useEffect(() => {
+    socket = io(ENDPOINT);
+
+    console.log(socket);
+  }, [ENDPOINT]);
+
+  return (
+    <Container>
+      <div className="left">
+        <ContactList />
+      </div>
+      <div className="right">
+        <Conversation />
+      </div>
+    </Container>
+  );
+};
 
 export default Main;
